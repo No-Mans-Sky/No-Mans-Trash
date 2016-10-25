98bb5d76c0866dc0a2f2e1fc100026847cc42e90
No-Mans-Sky <xxwallaberxx@gmail.com>
2016-10-25 08:18:02 -0400

    Initial upload

---
+++ GradeAnalyzer.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+class GradeAnalyzer{
+  public int getAverage(ArrayList<Integer> grades){
+if(grades.size() < 1){
+  System.out.println("ArrayList is empty");
+  return 0;
+}
+else{
+  int sum = 0;
+  for (Integer grade : grades) {
+    sum += grade;
+  }
+  int average = sum / grades.size();
+  System.out.println("The average of all the grades is " + average);
+  return average;
+}
+  }
+  public static void main(String[] args) {
+ArrayList<Integer> myClassroom = new ArrayList<Integer>();
+myClassroom.add(98);
+myClassroom.add(92);
+myClassroom.add(88);
+myClassroom.add(75);
+myClassroom.add(61);
+myClassroom.add(89);
+myClassroom.add(95);
+
+GradeAnalyzer myAnalyzer = new GradeAnalyzer();
+  myAnalyzer.getAverage(myClassroom);
+
+  }
+}
